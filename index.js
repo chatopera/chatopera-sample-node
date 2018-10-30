459da357f735b5a0b29a739daef4262e748dffbe
mukaiu <mukaiu@live.cn>
2018-10-30 11:56:17 +0800

    调整userId

--- index.js
+++ index.js
@@ -8,25 +8,29 @@ program
   .version('0.0.1')
   .arguments('<clientId> <secret>')
   .action((clientId, secret) => {
-    let client = new Chatopera(clientId, secret);
+    if (clientId && secret) {
+      let client = new Chatopera(clientId, secret);
 
-    let prompt = () => {
-      inquirer
-        .prompt({ name: 'send', message: 'Text' })
-        .then(function(answers) {
-          client
-            .conversation('Xiao', answers.send)
-            .then(res => {
-              console.log(res);
-              console.log('Bot:', res.string);
-            })
-            .catch(console.error)
-            .then(() => {
-              prompt();
-            });
-        });
-    };
+      let prompt = () => {
+        inquirer
+          .prompt({ name: 'send', message: 'Text' })
+          .then(function(answers) {
+            client
+              .conversation('xiao', answers.send)
+              .then(res => {
+                console.log(res);
+                console.log('Bot:', res.string);
+              })
+              .catch(console.error)
+              .then(() => {
+                prompt();
+              });
+          });
+      };
 
-    prompt();
+      prompt();
+    } else {
+      console.error('clientId and secret is required');
+    }
   })
   .parse(process.argv);
